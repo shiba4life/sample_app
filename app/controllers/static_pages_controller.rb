b8eb37e5cac3c6bdf83098926f3ea34ab20fb1be
Tom Tang <tomtang2@gmail.com>
2013-05-02 11:44:25 -0400

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @title = 'Home'
   end
 
   def about
-    @title = "About"
   end
   
   def help
-    @title = "Help"
+  end
+
+  def contact
   end
 end
